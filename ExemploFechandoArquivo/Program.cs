33772693678529841bf61a8c82f360b2d38f0ecc
virtualkami <kamillyvfq@gmail.com>
2022-11-23 15:10:22 -0400

    Adicionar arquivos de projeto.

---
+++ ExemploFechandoArquivo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExemploFechandoArquivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream entrada = null;
+            StreamReader leitor = null;
+            try { 
+                entrada = File.Open(@"c:\\windows\\temp\\aida.txt", FileMode.Open);
+                leitor = new StreamReader(entrada);
+                string linhas = leitor.ReadToEnd();
+                Console.WriteLine(linhas);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+            finally { 
+                if (leitor != null) { leitor.Close(); }
+                if (entrada != null) { entrada.Close(); }
+            }
+        }
+    }
+}
